e0f6b6a476e1dcdcf615a8cd70e666d45aaae68d
Emmanuel Benson <emmanuel.c.benson@gmail.com>
2018-12-12 09:28:12 +0000

    Changed page title

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -9,7 +9,7 @@
 
 <head>
     <meta charset="utf-8" />
-    <title>GPI Africa | Account</title>
+    <title>GPI Market Africa | Account</title>
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta content="width=device-width, initial-scale=1" name="viewport" />
     <meta content=" " name="description" />
