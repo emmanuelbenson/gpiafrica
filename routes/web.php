3e905cd9ab6954e32a1fa931c22535028d9409d9
Emmanuel Benson <emmanuel.c.benson@gmail.com>
2018-12-12 04:51:05 +0000

    Added endpoint for listing and adding countries where company's services are offered

--- routes/web.php
+++ routes/web.php
@@ -24,11 +24,15 @@ Route::get('/home/profile/{company}', 'CompanyController@profile')->name('compan
 
 
 Route::get('/profile', 'ProfileController@show');
+
 Route::get('/profile/edit-info', 'CompanyController@editInfo')->name('company.profile.info.edit');
 Route::put('/profile/update-info', 'CompanyController@updateInfo')->name('company.profile.info.update');
 Route::get('/profile/products', 'CompanyController@editProducts')->name('company.profile.products');
 Route::post('/profile/products/new', 'CompanyController@newProduct')->name('company.profile.product.new');
 Route::post('/profile/products', 'CompanyController@delProduct')->name('company.profile.product.delete');
+
+Route::get('/profile/service-countries', 'CompanyController@serviceCountries')->name('company.profile.service.countries');
+Route::post('/profile/service-countries/add', 'CompanyController@addCountries')->name('company.profile.add.countries');
 
 Route::post('/products', 'CompanyController@products')->name('company.products');
 
