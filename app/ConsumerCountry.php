9f645479a3f84610e9b91d89383306ff9928f4ac
Emmanuel Benson <emmanuel.c.benson@gmail.com>
2018-12-12 04:47:10 +0000

    Listed models fields in the model

--- app/ConsumerCountry.php
+++ app/ConsumerCountry.php
@@ -6,5 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class ConsumerCountry extends Model
 {
-    //
+    protected $fillable = [ 'company_id', 'country_id', 'product_id'];
+
+    public function countries()
+    {
+        return $this->hasMany('App\Country', 'id', 'country_id');
+    }
 }
